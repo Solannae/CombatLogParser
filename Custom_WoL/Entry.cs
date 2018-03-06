677caa4fa9ddd035f6144ba9639a75a61a0c23a1
Solannae <16961033+Solannae@users.noreply.github.com>
2018-03-06 16:07:31 +0100

    Repo Init

---
+++ Custom_WoL/Entry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_WoL
+{
+    class Entity
+    {
+        public int Guid { get; set; }
+        public string Name { get; set; }
+        public int Flags { get; set; }
+        public int Flags2 { get; set; }
+    }
+
+    class Spell
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int School { get; set; }
+    }
+
+    class Entry
+    {
+        public DateTime Timestamp { get; set; }
+        public Entity SourceEntity { get; set; }
+        public Entity DestEntity { get; set; }
+    }
+}
