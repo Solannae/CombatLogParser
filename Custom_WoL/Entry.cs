ea19a654fb0376789799ee02a2118ed76d52c2dd
Solannae <16961033+Solannae@users.noreply.github.com>
2018-03-06 20:36:32 +0100

    Filled basic interface to begin on parser work

--- Custom_WoL/Entry.cs
+++ Custom_WoL/Entry.cs
@@ -6,25 +6,78 @@ using System.Threading.Tasks;
 
 namespace Custom_WoL
 {
-    class Entity
+    public enum EventPrefix
     {
-        public int Guid { get; set; }
+        SWING,
+        RANGE,
+        SPELL,
+        SPELL_PERIODIC,
+        SPELL_BUILDING,
+        ENVIRONMENTAL
+    };
+
+    public enum EventSuffix
+    {
+        DAMAGE,
+        MISSED,
+        HEAL,
+        ENERGIZE,
+        DRAIN,
+        LEECH,
+        INTERRUPT,
+        DISPEL,
+        DISPEL_FAILED,
+        STOLEN,
+        EXTRA_ATTACKS,
+        AURA_APPLIED,
+        AURA_REMOVED,
+        AURA_APPLIED_DOSE,
+        AURA_REMOVED_DOSE,
+        AURA_REFRESH,
+        AURA_BROKEN,
+        AURA_BROKEN_SPELL,
+        CAST_START,
+        CAST_SUCCESS,
+        CAST_FAILED,
+        INSTAKILL,
+        DURABILITY_DAMAGE,
+        DURABILITY_DAMAGE_ALL,
+        CREATE,
+        SUMMON,
+        RESURRECT
+    };
+
+    public enum AuraType
+    {
+        BUFF,
+        DEBUFF
+    };
+
+    public class Entity
+    {
+        public long Guid { get; set; }
         public string Name { get; set; }
         public int Flags { get; set; }
         public int Flags2 { get; set; }
     }
 
-    class Spell
+    public class Spell
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public int School { get; set; }
     }
 
-    class Entry
+    public class Entry
     {
         public DateTime Timestamp { get; set; }
+        public string Type { get; set; }
+        public EventPrefix Prefix { get; set; }
+        public EventSuffix Suffix { get; set; }
         public Entity SourceEntity { get; set; }
         public Entity DestEntity { get; set; }
+        public Spell SpellCast { get; set; }
+        public Spell ExtraSpellCast { get; set; }
+        public AuraType Aura { get; set; }
     }
 }
