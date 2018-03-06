aff0e994167489fff5e85ee087b3ccbdab34021e
Solannae <16961033+Solannae@users.noreply.github.com>
2018-03-06 21:38:51 +0100

    Most of suffix management and flags assignment

--- Custom_WoL/Entry.cs
+++ Custom_WoL/Entry.cs
@@ -47,6 +47,20 @@ namespace Custom_WoL
         RESURRECT
     };
 
+    public enum MissType
+    {
+        ABSORB,
+        BLOCK,
+        DEFLECT,
+        DODGE,
+        EVADE,
+        IMMUNE,
+        MISS,
+        PARRY,
+        REFLECT,
+        RESIST
+    };
+
     public enum AuraType
     {
         BUFF,
@@ -93,6 +107,27 @@ namespace Custom_WoL
         }
     }
 
+    public class DamageInfo
+    {
+        public int Amount { get; set; }
+        public int Overkill { get; set; }
+        public int School { get; set; }
+        public int Resisted { get; set; }
+        public int Blocked { get; set; }
+        public int Absorbed { get; set; }
+        public bool Critical { get; set; }
+        public bool Glancing { get; set; }
+        public bool Crushing { get; set; }
+    }
+
+    public class HealingInfo
+    {
+        public int Amount { get; set; }
+        public int OverHealing { get; set; }
+        public int Absorbed { get; set; }
+        public bool Critical { get; set; }
+    }
+
     public class Entry
     {
         public DateTime Timestamp { get; set; }
@@ -103,8 +138,11 @@ namespace Custom_WoL
         public Entity DestEntity { get; set; }
         public Spell SpellCast { get; set; }
         public Spell ExtraSpellCast { get; set; }
+        public DamageInfo Damage { get; set; }
+        public HealingInfo Healing { get; set; }
         public EnvironmentalType Environment { get; set; }
         public AuraType Aura { get; set; }
+        public int Amount { get; set; }
 
         public void FillBasic(string[] tokens)
         {
@@ -125,6 +163,63 @@ namespace Custom_WoL
                 case EventPrefix.ENVIRONMENTAL:
                     Enum.TryParse(tokens[9], out EnvironmentalType Environment);
                     break;
+                default:
+                    break;
+            }
+        }
+
+        public void FillSuffixFlags(string[] tokens)
+        {
+            AuraType tmp_aura;
+            switch (Suffix)
+            {
+                case EventSuffix.DAMAGE:
+                    Damage = new DamageInfo();   //TODO
+                    break;
+
+                case EventSuffix.MISSED:         //TODO
+                    break;
+
+                case EventSuffix.HEAL:
+                    Healing = new HealingInfo(); //TODO
+                    break;
+
+                case EventSuffix.DRAIN:
+                case EventSuffix.LEECH:
+                    break;                       //TODO
+
+                case EventSuffix.INTERRUPT:
+                case EventSuffix.DISPEL_FAILED:
+                    ExtraSpellCast = new Spell(int.Parse(tokens[12]), tokens[13], int.Parse(tokens[14]));
+                    break;
+
+                case EventSuffix.EXTRA_ATTACKS:
+                    Amount = int.Parse(tokens[12]);
+                    break;
+
+                case EventSuffix.DISPEL:
+                case EventSuffix.STOLEN:
+                case EventSuffix.AURA_BROKEN_SPELL:
+                    ExtraSpellCast = new Spell(int.Parse(tokens[12]), tokens[13], int.Parse(tokens[14]));
+                    Enum.TryParse(tokens[15], out tmp_aura);
+                    Aura = tmp_aura;
+                    break;
+
+                case EventSuffix.AURA_APPLIED:
+                case EventSuffix.AURA_REMOVED:
+                case EventSuffix.AURA_REFRESH:
+                case EventSuffix.AURA_BROKEN:
+                    Enum.TryParse(tokens[12], out tmp_aura);
+                    Aura = tmp_aura;
+                    break;
+
+                case EventSuffix.AURA_APPLIED_DOSE:
+                case EventSuffix.AURA_REMOVED_DOSE:
+                    Enum.TryParse(tokens[12], out tmp_aura);
+                    Aura = tmp_aura;
+                    Amount = int.Parse(tokens[13]);
+                    break;
+
                 default:
                     break;
             }
