1245754dc97e7f2082f72a4e68aadbcb10128785
Solannae <16961033+Solannae@users.noreply.github.com>
2018-03-08 13:22:52 +0100

    Adapt Entry.GUID to corresponding structure

--- Custom_WoL/Entry.cs
+++ Custom_WoL/Entry.cs
@@ -6,205 +6,6 @@ using System.Threading.Tasks;
 
 namespace Custom_WoL
 {
-    public enum EventPrefix
-    {
-        SWING,
-        RANGE,
-        SPELL,
-        SPELL_PERIODIC,
-        SPELL_BUILDING,
-        ENVIRONMENTAL,
-        ENCHANT,
-        PARTY,
-        UNIT
-    };
-
-    public enum EventSuffix
-    {
-        DAMAGE,
-        MISSED,
-        HEAL,
-        ENERGIZE,
-        DRAIN,
-        LEECH,
-        INTERRUPT,
-        DISPEL,
-        DISPEL_FAILED,
-        STOLEN,
-        EXTRA_ATTACKS,
-        AURA_APPLIED,
-        AURA_REMOVED,
-        AURA_APPLIED_DOSE,
-        AURA_REMOVED_DOSE,
-        AURA_REFRESH,
-        AURA_BROKEN,
-        AURA_BROKEN_SPELL,
-        CAST_START,
-        CAST_SUCCESS,
-        CAST_FAILED,
-        INSTAKILL,
-        DURABILITY_DAMAGE,
-        DURABILITY_DAMAGE_ALL,
-        CREATE,
-        SUMMON,
-        RESURRECT,
-        KILL,
-        DIED,
-        DESTROYED,
-        APPLIED,
-        REMOVED
-    };
-
-    public enum MissType
-    {
-        ABSORB,
-        BLOCK,
-        DEFLECT,
-        DODGE,
-        EVADE,
-        IMMUNE,
-        MISS,
-        PARRY,
-        REFLECT,
-        RESIST
-    };
-
-    public enum AuraType
-    {
-        BUFF,
-        DEBUFF
-    };
-
-    public enum PowerType
-    {
-        HEALTH = -2,
-        MANA = 0,
-        RAGE = 1,
-        FOCUS = 2,
-        ENERGY = 3,
-        COMBO_POINTS = 4,
-        RUNES = 5,
-        RUNIC_POWER = 6,
-        SOUL_SHARDS = 7,
-        LUNAR_POWER = 8,
-        HOLY_POWER = 9,
-        ALTERNATE_POWER = 10,
-        MAELSTROM = 11,
-        CHI = 12,
-        INSANITY = 13,
-        OBSOLETE = 14,
-        OBSOLETE_2 = 15,
-        ARCANE_CHARGES = 16,
-        FURY = 17,
-        PAIN = 18
-    };
-
-    public enum EnvironmentalType
-    {
-        DROWNING,
-        FALLING,
-        FATIGUE,
-        FIRE,
-        LAVA,
-        SLIME
-    };
-
-    public class Entity
-    {
-        public long Guid { get; set; }
-        public string Name { get; set; }
-        public int Flags { get; set; }
-        public int Flags2 { get; set; }
-
-        public Entity(long _guid, string _name, int _flags, int _flags2)
-        {
-            Guid = _guid;
-            Name = _name;
-            Flags = _flags;
-            Flags2 = _flags2;
-        }
-    }
-
-    public class Spell
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public int School { get; set; }
-
-        public Spell(int _id, string _name, int _school)
-        {
-            ID = _id;
-            Name = _name;
-            School = _school;
-        }
-    }
-
-    public class DamageInfo
-    {
-        public int Amount { get; set; }
-        public int Overkill { get; set; }
-        public int School { get; set; }
-        public int Resisted { get; set; }
-        public int Blocked { get; set; }
-        public int Absorbed { get; set; }
-        public bool Critical { get; set; }
-        public bool Glancing { get; set; }
-        public bool Crushing { get; set; }
-
-        public DamageInfo(int _amount, int _overkill, int _school, int _res,
-                          int _block, int _abs, bool _crit, bool _glance, bool _crush)
-        {
-            Amount = _amount;
-            Overkill = _overkill;
-            School = _school;
-            Resisted = _res;
-            Blocked = _block;
-            Absorbed = _abs;
-            Critical = _crit;
-            Glancing = _glance;
-            Crushing = _crush;
-        }
-    }
-
-    public class HealingInfo
-    {
-        public int Amount { get; set; }
-        public int OverHealing { get; set; }
-        public int Absorbed { get; set; }
-        public bool Critical { get; set; }
-
-        public HealingInfo(int _amount, int _over, int _abs, bool _crit)
-        {
-            Amount = _amount;
-            OverHealing = _over;
-            Absorbed = _abs;
-            Critical = _crit;
-        }
-    }
-
-    public class MissedInfo
-    {
-        public MissType TypeMiss { get; set; }
-        public bool IsOffHand { get; set; }
-        public int AmountMissed { get; set; }
-
-        public MissedInfo(MissType _type)
-        {
-            TypeMiss = _type;
-        }
-
-        public MissedInfo(int _amount)
-        {
-            AmountMissed = _amount;
-        }
-
-        public MissedInfo(MissType _type, int _amount)
-        {
-            TypeMiss = _type; 
-            AmountMissed = _amount;
-        }
-    }
-
     public class Entry
     {
         public DateTime Timestamp { get; set; }
@@ -224,6 +25,8 @@ namespace Custom_WoL
         public int Amount { get; set; }
         public int ExtraAmount { get; set; }
 
+        
+
         public Entry(DateTime _time)
         {
             Timestamp = _time;
@@ -236,10 +39,10 @@ namespace Custom_WoL
             return int.Parse(nb, System.Globalization.NumberStyles.HexNumber);
         }
 
-        public long HexaToLong(string nb)
+        public ulong HexaToLong(string nb)
         {
             nb = nb.Substring(2);
-            return long.Parse(nb, System.Globalization.NumberStyles.HexNumber);
+            return ulong.Parse(nb, System.Globalization.NumberStyles.HexNumber);
         }
 
         public bool ToBool(string token)
@@ -266,6 +69,7 @@ namespace Custom_WoL
 
         public void FindPrefix(string event_type)
         {
+            //Three specific cases where an event type must be considered as another one
             if (event_type == "DAMAGE_SHIELD" || event_type == "DAMAGE_SPLIT" || event_type == "DAMAGE_SHIELD_MISSED")
                 Prefix = EventPrefix.SPELL;
             else if (event_type.Contains("SWING"))
@@ -290,6 +94,7 @@ namespace Custom_WoL
 
         public void FindSuffix(string event_type)
         {
+            //Three specific cases where an event type must be considered as another one
             if (event_type == "DAMAGE_TYPE" || event_type == "DAMAGE_SPLIT")
                 Suffix = EventSuffix.DAMAGE;
             else if (event_type == "DAMAGE_SHIELD_MISSED")
@@ -343,13 +148,13 @@ namespace Custom_WoL
                 case EventSuffix.MISSED:
                     if (Prefix == EventPrefix.SWING)
                     {
-                        Enum.TryParse(tokens[9], out MissType miss);
+                        Enum.TryParse(tokens[9], out MissedInfo.MissType miss);
                         switch (miss)
                         {
-                            case MissType.DODGE:
-                            case MissType.IMMUNE:
-                            case MissType.MISS:
-                            case MissType.PARRY:
+                            case MissedInfo.MissType.DODGE:
+                            case MissedInfo.MissType.IMMUNE:
+                            case MissedInfo.MissType.MISS:
+                            case MissedInfo.MissType.PARRY:
                                 Missed = new MissedInfo(miss);
                                 break;
                             default:
@@ -359,16 +164,16 @@ namespace Custom_WoL
                     }
                     else
                     {
-                        Enum.TryParse(tokens[12], out MissType miss);
+                        Enum.TryParse(tokens[12], out MissedInfo.MissType miss);
                         switch (miss)
                         {
-                            case MissType.DEFLECT:
-                            case MissType.DODGE:
-                            case MissType.EVADE:
-                            case MissType.IMMUNE:
-                            case MissType.MISS:
-                            case MissType.PARRY:
-                            case MissType.REFLECT:
+                            case MissedInfo.MissType.DEFLECT:
+                            case MissedInfo.MissType.DODGE:
+                            case MissedInfo.MissType.EVADE:
+                            case MissedInfo.MissType.IMMUNE:
+                            case MissedInfo.MissType.MISS:
+                            case MissedInfo.MissType.PARRY:
+                            case MissedInfo.MissType.REFLECT:
                                 Missed = new MissedInfo(miss);
                                 break;
                             default:
@@ -425,5 +230,94 @@ namespace Custom_WoL
                     break;
             }
         }
+
+        public enum EventPrefix
+        {
+            SWING,
+            RANGE,
+            SPELL,
+            SPELL_PERIODIC,
+            SPELL_BUILDING,
+            ENVIRONMENTAL,
+            ENCHANT,
+            PARTY,
+            UNIT
+        };
+
+        public enum EventSuffix
+        {
+            DAMAGE,
+            MISSED,
+            HEAL,
+            ENERGIZE,
+            DRAIN,
+            LEECH,
+            INTERRUPT,
+            DISPEL,
+            DISPEL_FAILED,
+            STOLEN,
+            EXTRA_ATTACKS,
+            AURA_APPLIED,
+            AURA_REMOVED,
+            AURA_APPLIED_DOSE,
+            AURA_REMOVED_DOSE,
+            AURA_REFRESH,
+            AURA_BROKEN,
+            AURA_BROKEN_SPELL,
+            CAST_START,
+            CAST_SUCCESS,
+            CAST_FAILED,
+            INSTAKILL,
+            DURABILITY_DAMAGE,
+            DURABILITY_DAMAGE_ALL,
+            CREATE,
+            SUMMON,
+            RESURRECT,
+            KILL,
+            DIED,
+            DESTROYED,
+            APPLIED,
+            REMOVED
+        };
+
+        public enum AuraType
+        {
+            BUFF,
+            DEBUFF
+        };
+
+        public enum PowerType
+        {
+            HEALTH = -2,
+            MANA = 0,
+            RAGE = 1,
+            FOCUS = 2,
+            ENERGY = 3,
+            COMBO_POINTS = 4,
+            RUNES = 5,
+            RUNIC_POWER = 6,
+            SOUL_SHARDS = 7,
+            LUNAR_POWER = 8,
+            HOLY_POWER = 9,
+            ALTERNATE_POWER = 10,
+            MAELSTROM = 11,
+            CHI = 12,
+            INSANITY = 13,
+            OBSOLETE = 14,
+            OBSOLETE_2 = 15,
+            ARCANE_CHARGES = 16,
+            FURY = 17,
+            PAIN = 18
+        };
+
+        public enum EnvironmentalType
+        {
+            DROWNING,
+            FALLING,
+            FATIGUE,
+            FIRE,
+            LAVA,
+            SLIME
+        };
     }
 }
