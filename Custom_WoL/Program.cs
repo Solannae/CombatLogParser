6a23cf6c57e06c531dd823d59138baf742bd72b5
Solannae <16961033+Solannae@users.noreply.github.com>
2018-03-08 16:26:10 +0100

    Refactor main code using new Parser method BuildEncountersList()

--- Custom_WoL/Program.cs
+++ Custom_WoL/Program.cs
@@ -9,65 +9,25 @@ using System.Threading.Tasks;
 
 namespace Custom_WoL
 {
-    public class Parser
-    {
-        public string Path { get; set; }
-
-        public Parser(string path)
-        {
-            Path = path;
-        }
-
-        public DateTime FormatDateTime(string time)
-        {
-            string date_part = time.Split(' ')[0];
-            string hour_part = time.Split(' ')[1];
-            return new DateTime(DateTime.Now.Year, int.Parse(date_part.Split('/')[0]), int.Parse(date_part.Split('/')[1]),
-                                int.Parse(hour_part.Split(':')[0]), int.Parse(hour_part.Split(':')[1]), int.Parse(hour_part.Split(':')[2].Split('.')[0]),
-                                int.Parse(hour_part.Split(':')[2].Split('.')[1]));
-        }
-
-        public Queue<Entry> Parse()
-        {
-            Queue<Entry> entries = new Queue<Entry>();
-            using (var file = File.OpenRead(Path))  
-            using (var reader = new StreamReader(file))
-            {
-                string token;
-                while ((token = reader.ReadLine()) != null)
-                {
-                    var timestamp = token.Split(new string[] { "  " }, 2, StringSplitOptions.RemoveEmptyEntries)[0];
-                    var arguments = Regex.Split(token.Split(new string[] { "  " }, 2, StringSplitOptions.RemoveEmptyEntries)[1], ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-                    Entry tmp_entry = new Entry(FormatDateTime(timestamp));
-                    tmp_entry.Fill(arguments);
-                    entries.Enqueue(tmp_entry);
-                }
-            }
-            return entries;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             string path = "Tests\\WoWCombatLog.txt";
-            Parser wol = new Parser(path);
-            Queue<Entry> entries = wol.Parse();
-            List<Encounter> encounters = new List<Encounter>();
-            
-            while (entries.Count != 0)  
-                encounters.Add(new Encounter(entries));
+            Parser parser = new Parser(path);
+            parser.Parse();
+            parser.BuildEncountersList();            
 
-            foreach (var fight in encounters)
+            foreach (var fight in parser.Encounters)
             {
                 Console.WriteLine();
                 Console.WriteLine("---------------------------------------------------");
-                Console.WriteLine("Begin : " + fight.Start);
-                Console.WriteLine("---------------------------------------------------\nDamage Taken :");
-                foreach (var enemy in fight.Enemies.Where(u => u.Value.DamageTaken != 0))
+                Console.WriteLine("Begin : " + fight.Start + ", End : " + fight.End);
+                Console.WriteLine("Fight : " + fight.NPC.OrderByDescending  (u => u.Value.DamageTaken).First().Key.Name);
+                Console.WriteLine("---------------------------------------------------\nDamage Done :");
+                foreach (var enemy in fight.Players.Where(u => u.Value.DamageDone != 0))
                 {
-                    Console.WriteLine(enemy.Key.Name + " Dakage Taken : " + enemy.Value.DamageTaken);
+                    Console.WriteLine(enemy.Key.Name + " Damage Done : " + enemy.Value.DamageDone);
                 }
 
             }
