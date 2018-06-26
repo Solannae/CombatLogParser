64b99d4a77771421609ef925cb9d3bc5c1b3a83c
Solannae <16961033+Solannae@users.noreply.github.com>
2018-06-26 16:11:03 +0200

    Constructor overload to build object from existing data

--- Custom_WoL/CombatInfo.cs
+++ Custom_WoL/CombatInfo.cs
@@ -19,5 +19,22 @@ namespace Custom_WoL
         {
             IsDead = true;
         }
+
+        public CombatInfo(long _dd, long _dt, long _hd, long _ht, bool _is_dead, DateTime _last_active)
+        {
+            DamageDone = _dd;
+            DamageTaken = _dt;
+            HealingDone = _hd;
+            HealingTaken = _ht;
+            IsDead = _is_dead;
+            LastActive = _last_active;
+        }
+
+        public static CombatInfo operator +(CombatInfo a, CombatInfo b)
+        {
+            return new CombatInfo(a.DamageDone + b.DamageDone, a.DamageTaken + b.DamageTaken,
+                                  a.HealingDone + b.HealingDone, a.HealingTaken + b.HealingTaken,
+                                  b.IsDead, b.LastActive);
+        }
     }
 }
