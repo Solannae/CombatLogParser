61f6055f9e01c42ac35116c73b4e96fafda55d77
Solannae <16961033+Solannae@users.noreply.github.com>
2019-11-15 16:31:43 +0100

    Removed unused includes

--- Custom_WoL/EntryInfos.cs
+++ Custom_WoL/EntryInfos.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Custom_WoL
+﻿namespace Custom_WoL
 {
     public class DamageInfo
     {
