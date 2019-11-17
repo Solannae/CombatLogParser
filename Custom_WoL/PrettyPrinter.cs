124a49a44994f8b8153e58e59a7c525fd63399ba
Solannae <16961033+Solannae@users.noreply.github.com>
2019-11-17 12:59:13 +0100

    Move every console output into dedicated Printer

---
+++ Custom_WoL/PrettyPrinter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_WoL
+{
+    class PrettyPrinter
+    {
+        public static void PrintEncountersInfos(List<Encounter> encounters)
+        {
+            foreach (var fight in encounters)
+            {
+                Console.WriteLine();
+                Console.WriteLine("---------------------------------------------------");
+                Console.WriteLine("Begin : " + fight.Start + ", End : " + fight.End);
+                Console.WriteLine("Fight : " + fight.NPC.OrderByDescending(u => u.Value.DamageTaken).First().Key.Name);
+                Console.WriteLine("---------------------------------------------------");
+                Console.WriteLine("Damage Done :");
+
+                foreach (var enemy in fight.Players.Where(u => u.Value.DamageDone != 0))
+                {
+                    Console.WriteLine(enemy.Key.Name + " Damage Done : " + enemy.Value.DamageDone);
+                }
+            }
+        }
+    }
+}
--- Custom_WoL/Program.cs
+++ Custom_WoL/Program.cs
@@ -10,22 +10,8 @@ namespace Custom_WoL
         {
             Parser parser = new Parser(ConfigurationManager.AppSettings["logPath"]);
             parser.Parse();
-            parser.BuildEncountersList();            
-
-            foreach (var fight in parser.Encounters)
-            {
-                Console.WriteLine();
-                Console.WriteLine("---------------------------------------------------");
-                Console.WriteLine("Begin : " + fight.Start + ", End : " + fight.End);
-                Console.WriteLine("Fight : " + fight.NPC.OrderByDescending  (u => u.Value.DamageTaken).First().Key.Name);
-                Console.WriteLine("---------------------------------------------------\nDamage Done :");
-                foreach (var enemy in fight.Players.Where(u => u.Value.DamageDone != 0))
-                {
-                    Console.WriteLine(enemy.Key.Name + " Damage Done : " + enemy.Value.DamageDone);
-                }
-
-            }
-
+            parser.BuildEncountersList();
+            PrettyPrinter.PrintEncountersInfos(parser.Encounters);
             Console.ReadKey();
         }
     }
